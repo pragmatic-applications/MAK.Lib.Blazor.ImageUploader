f67ff4ed017969e0ddb300ff87c708c5191040fd
Maurice Kelly <kay86@live.com>
2021-08-31 15:32:16 +0100

    Configure the App for NuGet

---
+++ docs/Record.cs
@@ -0,0 +1,21 @@
+//=================================================================
+// Solution: MAK.Lib.Blazor.ImageUploader
+//=================================================================
+// Programmer: Maurice Antonio Kelly @MAKMaurice
+// Description: A .NET 5.0 Blazor Razor Class Library wrapper for Blazor's built-in file uploader.
+// This library provides an easy-to-use component for uploading single or multiple files; it also provides instant visual user feedback of the file(s) uploaded.
+// The component provide Parameters for various options such as AcceptTypeList for the sccepted image types;
+// ToImageType for the image type to convert to, Css to set own style and Multiple to switch between single or multiple file choice.
+//
+// ASP.NET Core 5.0
+// Date: 2021-08-31
+// Stop: 16:00
+//=================================================================
+
+
+/*
+
+
+
+//*/
+
